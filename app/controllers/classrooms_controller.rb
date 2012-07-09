791abe7ce976c4f86b7c06aba5e114fb691ec3a2
Tom Mikota <javier.siado@koombea.com>
2012-07-08 20:30:13 -0500

    bootstrap

--- app/controllers/classrooms_controller.rb
+++ app/controllers/classrooms_controller.rb
@@ -1,16 +1,27 @@
 class ClassroomsController < ApplicationController
-	def teacher
-	end
+  def teacher
+  end
 
-	def student
-	end
+  def student
+  end
 
-	def playpause
-		if params[:video] == "0"
-		  PrivatePub.publish_to "/classroom/video", "document.getElementById('asset').play()"
-		elsif params[:video] == "1"
-		  PrivatePub.publish_to "/classroom/video", "document.getElementById('asset').pause()"
-		end
-	  render :nothing => true
-	end
+  def playpause
+    if params[:video] == "0"
+      PrivatePub.publish_to "/classroom/video", "document.getElementById('asset').play()"
+    elsif params[:video] == "1"
+      PrivatePub.publish_to "/classroom/video", "document.getElementById('asset').pause()"
+    end
+    render :nothing => true
+  end
+
+  def subscribe
+    @student_name = params[:name].capitalize
+  end
+
+  def message
+    time = Time.now.strftime("%H:%M")
+    message = params[:content]
+    PrivatePub.publish_to("/classroom/messages", {time: time, message: message})
+    render :nothing => true
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -19,6 +19,8 @@ Privatepub::Application.routes.draw do
         get 'teacher'
         get 'student'
         post 'playpause'
+        post 'subscribe', :format => :js
+        post 'message', :format => :js
       end
     end
 
@@ -45,7 +47,7 @@ Privatepub::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'classrooms#teacher'
+  root :to => 'classrooms#student'
 
   # See how all your routes lay out with "rake routes"
 
